51c1ed519b004cc35be5fbc08cd15530f3408786
Andy A <andy.075109@hotmail.co.uk>
2017-03-21 01:21:04 +0000

    Improved reliability and speed

--- app/src/main/java/com/baronkiko/launcherhijack/AccServ.java
+++ app/src/main/java/com/baronkiko/launcherhijack/AccServ.java
@@ -18,18 +18,22 @@ public class AccServ extends AccessibilityService {
         return new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME).setPackage(s).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS|Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
     }
 
+    private void PerformHomePress()
+    {
+        performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            Log.wtf(TAG,e);
+        }
+        performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+        startActivity(GetDesiredIntent());
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        if(event.getPackageName().equals("com.amazon.firelauncher")) {
-            performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                Log.wtf(TAG,e);
-            }
-            performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
-            startActivity(GetDesiredIntent());
-        }
+        if(event.getPackageName().equals("com.amazon.firelauncher"))
+            PerformHomePress();
     }
 
     @Override
@@ -47,14 +51,7 @@ public class AccServ extends AccessibilityService {
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
         info.packageNames = new String[]{"com.amazon.firelauncher"};
         setServiceInfo(info);
-performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                Log.wtf(TAG,e);
-            }
-            performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
-            startActivity(GetDesiredIntent());
+        PerformHomePress();
     }
 
 }
