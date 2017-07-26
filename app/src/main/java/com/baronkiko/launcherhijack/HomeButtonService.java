2dec1947520346a58ec4a36ee5cbb8c386cb4b1b
Andy Adshead <andy@Andys-iMac.mshome.net>
2017-07-26 22:11:52 +0100

    HomeButtonService fleshed out

--- app/src/main/java/com/baronkiko/launcherhijack/HomeButtonService.java
+++ app/src/main/java/com/baronkiko/launcherhijack/HomeButtonService.java
@@ -1,11 +1,19 @@
 package com.baronkiko.launcherhijack;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class HomeButtonService extends Service {
+    private LinearLayout layout;
+    private WindowManager wm;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -15,7 +23,33 @@ public class HomeButtonService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("A", "X");
+
+        layout =  new LinearLayout(getApplicationContext()) {
+            //home or recent button
+            public void onCloseSystemDialogs(String reason) {
+                if (reason.contains("homekey"))
+                    HomePress.Perform(getApplicationContext());
+            }
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event) {
+                return false;
+            }
+        };
+
+        layout.setFocusable(false);
+
+        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                0,
+                0,
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_FULLSCREEN
+                        | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.LEFT | Gravity.CENTER_VERTICAL;
+        wm.addView(layout, params);
     }
 
     @Override
@@ -28,6 +62,9 @@ public class HomeButtonService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.i("EXIT", "ondestroy!");
+
+        wm.removeView(layout);;
+
         ServiceMan.Stop(getApplicationContext());
         ServiceMan.StartSlow(getApplicationContext());
     }
