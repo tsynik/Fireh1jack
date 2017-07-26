4c7f6f2eb50b81a83f8e9b1a5eb8685156f3d481
Andy Adshead <andy@Andys-iMac.mshome.net>
2017-07-26 04:38:15 +0100

    New homepress method and static

--- app/src/main/java/com/baronkiko/launcherhijack/AccServ.java
+++ app/src/main/java/com/baronkiko/launcherhijack/AccServ.java
@@ -2,8 +2,6 @@ package com.baronkiko.launcherhijack;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -11,29 +9,10 @@ public class AccServ extends AccessibilityService {
 
     static final String TAG = "AccServ";
 
-    public Intent GetDesiredIntent()
-    {
-        SharedPreferences settings = getSharedPreferences("LauncherHijack", MODE_WORLD_READABLE);
-        String s = settings.getString("ChosenLauncher", "com.teslacoilsw.launcher");
-        return new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME).setPackage(s).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS|Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-    }
-
-    private void PerformHomePress()
-    {
-        performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            Log.wtf(TAG,e);
-        }
-        performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
-        startActivity(GetDesiredIntent());
-    }
-
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         if(event.getPackageName().equals("com.amazon.firelauncher"))
-            PerformHomePress();
+            HomePress.Perform(getApplicationContext());
     }
 
     @Override
@@ -51,7 +30,7 @@ public class AccServ extends AccessibilityService {
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
         info.packageNames = new String[]{"com.amazon.firelauncher"};
         setServiceInfo(info);
-        PerformHomePress();
+        HomePress.Perform(getApplicationContext());
     }
 
 }
---
+++ app/src/main/java/com/baronkiko/launcherhijack/HomePress.java
@@ -0,0 +1,34 @@
+package com.baronkiko.launcherhijack;
+
+import android.accessibilityservice.AccessibilityService;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import static android.os.ParcelFileDescriptor.MODE_WORLD_READABLE;
+
+/**
+ * Created by andy on 26/07/2017.
+ */
+
+public class HomePress {
+    public static Intent GetDesiredIntent(Context c)
+    {
+        SharedPreferences settings = c.getSharedPreferences("LauncherHijack", MODE_WORLD_READABLE);
+        String s = settings.getString("ChosenLauncher", "com.teslacoilsw.launcher");
+        return new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME).setPackage(s).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS|Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+    }
+
+    public static void Perform(Context c)
+    {
+        Intent i = GetDesiredIntent(c);
+        PendingIntent pendingIntent = PendingIntent.getActivity(c, 0, i, 0);
+        try {
+            pendingIntent.send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+    }
+}
