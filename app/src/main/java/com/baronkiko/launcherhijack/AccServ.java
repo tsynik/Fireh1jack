54e2f4ebff0dcb3d33c9158765d32ccd10f53a5a
Andy Adshead <andy@Andys-iMac.local>
2018-12-01 08:36:53 +0000

    Hold menu to override

--- app/src/main/java/com/baronkiko/launcherhijack/AccServ.java
+++ app/src/main/java/com/baronkiko/launcherhijack/AccServ.java
@@ -1,19 +1,21 @@
 package com.baronkiko.launcherhijack;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
+
 
 public class AccServ extends AccessibilityService {
 
     static final String TAG = "AccServ";
+    static boolean HomePressCanceled = false;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        CharSequence packageName = event.getPackageName();
+        /*CharSequence packageName = event.getPackageName();
         if(packageName.equals("com.amazon.firelauncher"))
-            HomePress.Perform(getApplicationContext());
+            HomePress.Perform(getApplicationContext());*/
     }
 
     @Override
@@ -22,12 +24,36 @@ public class AccServ extends AccessibilityService {
     }
 
     @Override
+    public boolean onKeyEvent(KeyEvent event)
+    {
+        switch (event.getKeyCode())
+        {
+            case KeyEvent.KEYCODE_HOME:
+                int action = event.getAction();
+
+                if (action == KeyEvent.ACTION_UP)
+                    HomePressCanceled = false;
+                else if (action == KeyEvent.ACTION_DOWN && !HomePressCanceled)
+                {
+                    HomePress.Perform(getApplicationContext());
+                    return true;
+                }
+
+                return false;
+
+            case KeyEvent.KEYCODE_MENU:
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                    HomePressCanceled = true;
+                return false;
+        }
+        return false;
+    }
+
+    @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
 
-        ServiceMan.Start(getApplicationContext());
-
-        Log.v(TAG, "onServiceConnected");
+        Log.v(TAG, "Launcher Hijack Service Started");
         HomePress.Perform(getApplicationContext());
     }
 
