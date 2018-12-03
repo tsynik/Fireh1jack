c4ce636b2dbc0b3dff624fbb9d6016ae357644a2
Andy Adshead <andy@Andys-iMac.local>
2018-12-03 22:05:00 +0000

    Temp disabled, causing issues on new fire 8

--- app/src/main/java/com/baronkiko/launcherhijack/HomeButtonService.java
+++ app/src/main/java/com/baronkiko/launcherhijack/HomeButtonService.java
@@ -25,33 +25,43 @@ public class HomeButtonService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        layout =  new LinearLayout(getApplicationContext()) {
-            //home or recent button
-            public void onCloseSystemDialogs(String reason) {
-                if (reason.contains("homekey"))
-                    HomePress.Perform(getApplicationContext());
-            }
+        // To be controlled with setting later
+        if (false)
+        {
+            layout = new LinearLayout(getApplicationContext())
+            {
+                //home or recent button
+                public void onCloseSystemDialogs(String reason)
+                {
+                    Log.d("HomeButtonService", "Close System Dialog: " + reason);
+                    if (reason.contains("homekey"))
+                        HomePress.Perform(getApplicationContext());
+                }
 
-            @Override
-            public boolean dispatchKeyEvent(KeyEvent event) {
-                return false;
-            }
-        };
+                @Override
+                public boolean dispatchKeyEvent(KeyEvent event)
+                {
+                    Log.d("HomeButtonService", "Dispatch Key Event");
 
-        layout.setFocusable(false);
+                    return false;
+                }
+            };
 
-        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            layout.setFocusable(false);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                0,
-                0,
-                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                        | WindowManager.LayoutParams.FLAG_FULLSCREEN
-                        | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-                PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.LEFT | Gravity.CENTER_VERTICAL;
-        wm.addView(layout, params);
+            wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    0,
+                    0,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                            | WindowManager.LayoutParams.FLAG_FULLSCREEN
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                    PixelFormat.TRANSLUCENT);
+            params.gravity = Gravity.LEFT | Gravity.CENTER_VERTICAL;
+            wm.addView(layout, params);
+        }
     }
 
     @Override
