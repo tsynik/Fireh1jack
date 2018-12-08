b66bbdc936338c7d985b9f9d686d86700e58f4dd
Andy Adshead <andy@Andys-iMac.local>
2018-12-08 21:03:53 +0000

    Resets package if chosen launcher is uninstalled

--- app/src/main/java/com/baronkiko/launcherhijack/BroadcastReceiverOnBootComplete.java
+++ app/src/main/java/com/baronkiko/launcherhijack/BroadcastReceiverOnBootComplete.java
@@ -3,15 +3,36 @@ package com.baronkiko.launcherhijack;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class BroadcastReceiverOnBootComplete extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        MainActivity.SetContext(context);
-        ServiceMan.Start(context);
+        switch (intent.getAction())
+        {
+            case Intent.ACTION_PACKAGE_REMOVED:
+                SharedPreferences settings = context.getSharedPreferences("LauncherHijack", MODE_PRIVATE);
+                String s = settings.getString("ChosenLauncher", "");
+
+                if (intent.getData().getSchemeSpecificPart().equals(s))
+                {
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString("ChosenLauncher", "com.baronkiko.launcherhijack");
+                    editor.putString("ChosenLauncherName", "com.baronkiko.launcherhijack.MainActivity");
+                    editor.commit(); // Commit the edits!
+                }
+                break;
+
+            case  Intent.ACTION_BOOT_COMPLETED:
+                MainActivity.SetContext(context);
+                ServiceMan.Start(context);
+                break;
+        }
     }
 
 
